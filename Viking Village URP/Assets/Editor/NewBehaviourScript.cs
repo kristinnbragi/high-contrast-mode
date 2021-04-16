f282fe849fbf05c652a405f4bff4317d43ba3526
Kristinn Bragi <kristinnbragi@gmail.com>
2021-04-16 11:58:55 +0200

    viking village removed

--- Viking Village URP/Assets/Editor/NewBehaviourScript.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SUPER NEAT THING", menuName = "SUPER/NEAT/THING", order = 100)]
-public class NewBehaviourScript : ScriptableObject
-{
- 
-}
